7ed965eadadd50c6b80f39b44673c74551f65716
Davidson Sousa <dev@davidsonsousa.net>
2019-02-14 15:15:10 +0100

    Added UI project

---
+++ CommentConvert.Ui/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace CommentConvert.Ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            btnProcessComments.Click += BtnProcessComments_Click;
+        }
+
+        private void BtnProcessComments_Click(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
