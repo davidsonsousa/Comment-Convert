b5647acfa7a6bc2e2562d929b18b49a70d9ac1df
Davidson Sousa <dev@davidsonsousa.net>
2019-02-14 16:07:07 +0100

    Added constants and utilities class

---
+++ CommentConvert.Ui/Core/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace CommentConvert.Ui.Core
+{
+    public class Constants
+    {
+        public const string GraphUrl = "https://graph.facebook.com/v2.6/?fields=og_object{{comments}}&id={0}&access_token={1}";
+    }
+}
---
+++ CommentConvert.Ui/Core/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace CommentConvert.Ui.Core
+{
+    public static class Utilities
+    {
+        public static string LoadFacebookComments(string articleUrl, string token)
+        {
+            string url = string.Format(Constants.GraphUrl, articleUrl, token);
+
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    return client.DownloadString(url);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
